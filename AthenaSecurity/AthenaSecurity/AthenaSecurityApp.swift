93efd0ab152b4f1f6520e5a951b1fe6b65930084
Tshimega Belmont <tshimegabelmont@gmail.com>
2022-11-21 08:31:18 +0200

    Initial Commit

---
+++ AthenaSecurity/AthenaSecurity/AthenaSecurityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AthenaSecurityApp.swift
+//  AthenaSecurity
+//
+//  Created by Tshimega Belmont on 2022/11/21.
+//
+
+import SwiftUI
+
+@main
+struct AthenaSecurityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
